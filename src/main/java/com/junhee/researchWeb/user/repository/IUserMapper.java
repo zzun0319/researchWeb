270617b0440644176772eff3ce1464481e2beb23
junheeHome <junhee0319@ajou.ac.kr>
2021-05-06 00:52:58 +0900

    jdbc, hikariCP, dataSource, myBatis, test 등 필요한 거 pon.xml에 추가해서 설치했고, UserVO생성 성, 회원가입까지 했는데 수정해야함, 학부생은 승인이 1로 바뀌도록 수정할 것, 동적SQL문 사용해서

---
+++ src/main/java/com/junhee/researchWeb/user/repository/IUserMapper.java
@@ -0,0 +1,26 @@
+package com.junhee.researchWeb.user.repository;
+
+import java.util.List;
+
+import com.junhee.researchWeb.user.model.UserVO;
+
+public interface IUserMapper {
+
+	// 회원 가입 기능
+	void Register(UserVO user);
+	
+	// 로그인 체크 기능 (가입 승인 여부도 체크해야함)
+	int LoginCheck(UserVO user);
+	
+	// 회원 정보 수정 기능
+	void UpdateUserInfo(UserVO user);
+	
+	// 비밀 번호 수정 기능
+	void UpdatePassword(String userId, String pw);
+	
+	// 회원 탈퇴 기능
+	void DeleteUser(String userId);
+	
+	// 회원 정보 조회 기능 (어느 날짜에 누가 신청했는지, 각 실험 스케줄의 id를 받는다)
+	//List<UserVO> GetParticipantList(int researchId);
+}
