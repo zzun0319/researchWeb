ded6c5774c42943e43dbb07bcf08acd2a84a8523
junheeHome <junhee0319@ajou.ac.kr>
2021-05-08 14:11:18 +0900

    file1추가

--- src/main/java/com/junhee/researchWeb/user/repository/IUserMapper.java
+++ src/main/java/com/junhee/researchWeb/user/repository/IUserMapper.java
@@ -1,6 +1,7 @@
 package com.junhee.researchWeb.user.repository;
 
-import java.util.List;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.junhee.researchWeb.user.model.UserVO;
 
@@ -10,13 +11,14 @@ public interface IUserMapper {
 	void Register(UserVO user);
 	
 	// 로그인 체크 기능 (가입 승인 여부도 체크해야함)
-	int LoginCheck(UserVO user);
+	UserVO getOneUserInfo(String userId);
 	
 	// 회원 정보 수정 기능
 	void UpdateUserInfo(UserVO user);
 	
 	// 비밀 번호 수정 기능
-	void UpdatePassword(String userId, String pw);
+	void UpdatePassword(@Param("userId") String userId, @Param("userPw") String userPw);
+	// MyBatis는 파라미터를 2개 이상 받을 수 없음. @Param 쓰거나 Map<String,String> 을 이용해서 받거나, 그냥 객체를 통째로 받아야함.
 	
 	// 회원 탈퇴 기능
 	void DeleteUser(String userId);
